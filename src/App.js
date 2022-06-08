6801322c9548e2ed1a8c46cf117aa925fcd0be6a
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-06-09 01:29:11 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Header from './components/Shared/Header';
 
 function App() {
   return (
     <div className="App">
-      
+       <Header />
+       <Routes>
+         <Route path='/' element={<Home />}></Route>
+       </Routes>
     </div>
   );
 }
---
+++ src/components/Shared/Header.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { AiOutlineMenu } from 'react-icons/ai';
+import { Link } from "react-router-dom";
+
+function Header() {
+    const [navbarOpen, setNavbarOpen] = useState(false);
+    
+    return (
+        <>
+
+            <nav className="relative flex flex-wrap items-center justify-between py-6 bg-[#F5F9FF] px-20 fixed">
+                <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                    <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+                        <Link
+                            className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-black"
+                            to="/"
+                        >
+                            Portfolio
+                        </Link>
+                        <button
+                            className="text-black cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg- block lg:hidden outline-none focus:outline-none w"
+                            type="button"
+                            onClick={() => setNavbarOpen(!navbarOpen)}
+                        >
+                            <AiOutlineMenu color="black" />
+                        </button>
+                    </div>
+                    <div
+                        className={
+                            "lg:flex flex-grow items-center" +
+                            (navbarOpen ? " flex" : " hidden")
+                        }
+                        id="example-navbar-danger"
+                    >
+                        <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+                            <li className="nav-item">
+                                <Link
+                                    className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-black hover:opacity-75"
+                                    to="/"
+                                >
+                                    <span className="ml-2">Home</span>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link
+                                    className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-black hover:opacity-75"
+                                    to="/blogs"
+                                >
+                                    <span className="ml-2">Blogs</span>
+                                </Link>
+                            </li>
+                           
+                            <li className="nav-item">
+                                <Link
+                                    className="px-3 py-2 flex items-center text-sm uppercase font-bold leading-snug text-black hover:opacity-75"
+                                    to="/inventory"
+                                >
+                                    <span className="ml-2">Projects</span>
+                                </Link>
+                            </li>
+                           
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+}
+export default Header;
